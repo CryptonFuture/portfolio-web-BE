41ebd1ef8c3c633391fde81f535a261250ec1c9b
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2024-12-26 13:55:39 +0500

    to add route file endpoint register api

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const authRoute = require('./routes/authRoutes')
 
 const app = express()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true, limit: '5mb'}))
 app.use(cors())
+
+app.use('v1/api/auth', authRoute)
 
 app.get('/', () => {
     console.log('Service is working');
