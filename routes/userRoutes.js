d97fc443f7f8b956dd1c60b208e9b1ddb67b87b5
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2025-01-09 11:52:08 +0500

    to add User Routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -4,5 +4,11 @@ const userController = require('../controllers/user/userController')
 const router = express()
 
 router.put('/changePassword', userController.changePassword)
+router.get('/getUser', userController.getUser)
+router.delete('/deleteUser/:id', userController.deleteUser)
+router.put('/deleteUsers/:id', userController.deleteUsers)
+router.get('/countUser', userController.countUser)
+
+
 
 module.exports = router
