6f806f4a1ff0b7b6416f04a407ad307bb7a1ba4f
CryptonFuture <86903729+CryptonFuture@users.noreply.github.com>
2025-01-13 16:46:52 +0500

    uploader image in admin register

--- controllers/admin/AdminRegisterController.js
+++ controllers/admin/AdminRegisterController.js
@@ -1,77 +1,87 @@
 const UserAdmin = require('../../models/userAdminSchema')
 const validator = require('validator')
 const bcrypt = require('bcryptjs')
+const upload = require('../../middleware/adminMulterConfig')
 
 const AdminRegister = async (req, res) => {
-    try {
-        const { firstname, lastname, email, password, confirmPass, phone, address } = req.body
-
-        if (!firstname || !lastname || !email || !password || !confirmPass) {
+    upload.single('AdminProfileImage')(req, res, async (err) => {
+        if (err) {
             return res.status(400).json({
                 success: false,
-                error: 'please fill out all fields'
-            })
+                error: err.message,
+            });
         }
+        try {
+            const { firstname, lastname, email, password, confirmPass, phone, address } = req.body
 
-        if (!validator.isEmail(email)) {
-            return res.status(400).json({
-                success: false,
-                error: 'Invalid email'
+            if (!firstname || !lastname || !email || !password || !confirmPass) {
+                return res.status(400).json({
+                    success: false,
+                    error: 'please fill out all fields'
+                })
+            }
+
+            if (!validator.isEmail(email)) {
+                return res.status(400).json({
+                    success: false,
+                    error: 'Invalid email'
+                })
+            }
+
+            const isExistUser = await UserAdmin.findOne({ email })
+
+            if (isExistUser) {
+                return res.status(400).json({
+                    success: false,
+                    error: 'Email already exists has been taken!'
+                })
+            } else if (password !== confirmPass) {
+                return res.status(400).json({
+                    success: false,
+                    error: "password does'nt match",
+                })
+            }
+
+            if (password.length < 10 || confirmPass.length < 10) {
+                return res.status(400).json({
+                    success: false,
+                    error: "Password must be at least 10 characters long",
+                })
+            }
+
+            const hashPassword = await bcrypt.hash(password, 10)
+            const hashConfirmPassword = await bcrypt.hash(confirmPass, 10)
+
+            const user = new UserAdmin({
+                firstname,
+                lastname,
+                email,
+                password: hashPassword,
+                confirmPass: hashConfirmPassword,
+                phone: phone ? phone : null,
+                address: address ? address : null,
+                AdminProfileImage: req.file ? req.file.path : null,
+                created_by: firstname + ' ' + lastname,
+                updated_by: firstname + ' ' + lastname,
+
             })
+
+            const userData = await user.save()
+
+            return res.status(200).json({
+                success: true,
+                message: "user admin create successfully",
+                data: userData
+            })
+        } catch (error) {
+            console.log(error);
+
+            return res.status(500).json({
+                success: false,
+                error: "internal server error"
+            });
         }
-
-        const isExistUser = await UserAdmin.findOne({ email })
-
-        if (isExistUser) {
-            return res.status(400).json({
-                success: false,
-                error: 'Email already exists has been taken!'
-            })
-        } else if (password !== confirmPass) {
-            return res.status(400).json({
-                success: false,
-                error: "password does'nt match",
-            })
-        }
-
-        if (password.length < 10 || confirmPass.length < 10) {
-            return res.status(400).json({
-                success: false,
-                error: "Password must be at least 10 characters long",
-            })
-        }
-
-        const hashPassword = await bcrypt.hash(password, 10)
-        const hashConfirmPassword = await bcrypt.hash(confirmPass, 10)
-
-        const user = new UserAdmin({
-            firstname,
-            lastname,
-            email,
-            password: hashPassword,
-            confirmPass: hashConfirmPassword,
-            phone: phone ? phone : null,
-            address: address ? address : null,
-            created_by: firstname + ' ' + lastname,
-            updated_by: firstname + ' ' + lastname,
-
-        })
-
-        const userData = await user.save()
-
-        return res.status(200).json({
-            success: true,
-            message: "user admin create successfully",
-            data: userData
-        })
-    } catch (error) {
-        console.log(error);
-
-        return res.status(500).json({
-            success: false,
-            error: "internal server error"
-        });
-    }
+    })
 }
 
 module.exports = {
